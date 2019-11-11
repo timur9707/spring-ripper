ace080e7d90c9bb9d8c6acd7b09d8d166e92a2ae
timur9707 <timur9707@mail.ru>
2019-11-11 19:51:55 +0300

    Custom scope

--- src/main/java/com/epam/screensaver/Config.java
+++ src/main/java/com/epam/screensaver/Config.java
@@ -10,7 +10,7 @@ import java.util.Random;
 public class Config {
 
     @Bean
-    @Scope("prototype")
+    @Scope("periodical")
     public Color color() {
         Random random = new Random();
         return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
