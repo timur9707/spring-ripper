ba539a9f0a4cbbbb159a0601f21d5f7ec5fd69b5
timur9707 <timur9707@mail.ru>
2019-11-11 18:52:57 +0300

    Color prototype scope

---
+++ src/main/java/com/epam/screensaver/ColorFrame.java
@@ -0,0 +1,28 @@
+package com.epam.screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Service
+public abstract class ColorFrame extends JFrame {
+
+    public ColorFrame() {
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace() {
+        Random random = new Random();
+        setLocation(random.nextInt(1200), random.nextInt(700));
+        getContentPane().setBackground(getColor());
+        repaint();
+    }
+
+    protected abstract Color getColor();
+
+}
